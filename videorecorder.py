d60119d2c5a322bab67fc93202ce25a493f79369
dek <dakoner@gmail.com>
2022-02-09 17:42:58 -0800

    add code to record video

---
+++ videorecorder.py
@@ -0,0 +1,14 @@
+import cv2
+import imagezmq
+import simplejpeg
+IMAGEZMQ='inspectionscope.local'
+
+out = cv2.VideoWriter('outpy.mkv',cv2.VideoWriter_fourcc(*'XVID'), 24, (1600, 1200))
+url = f"tcp://{IMAGEZMQ}:5555"
+image_hub = imagezmq.ImageHub(url, REQ_REP=False)
+
+while True:
+    name, jpg_buffer = image_hub.recv_jpg()
+    print(name)
+    image= simplejpeg.decode_jpeg( jpg_buffer, colorspace='RGB')
+    out.write(image)
