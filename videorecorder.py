f081c367fa0d47375b9ba3d2a4862ef504c4ef65
dek <dakoner@gmail.com>
2022-03-12 16:37:50 +0000

    don't open video til first frame to get size

--- videorecorder.py
+++ videorecorder.py
@@ -1,14 +1,15 @@
 import cv2
 import imagezmq
 import simplejpeg
-IMAGEZMQ='inspectionscope.local'
-
-out = cv2.VideoWriter('outpy.mkv',cv2.VideoWriter_fourcc(*'XVID'), 24, (1600, 1200))
-url = f"tcp://{IMAGEZMQ}:5556"
+IMAGEZMQ='gork.local'
+PORT=5556
+url = f"tcp://{IMAGEZMQ}:{PORT}"
 image_hub = imagezmq.ImageHub(url, REQ_REP=False)
 
+out = None
 while True:
     name, jpg_buffer = image_hub.recv_jpg()
-    print(name)
-    image= simplejpeg.decode_jpeg( jpg_buffer, colorspace='RGB')
+    image= simplejpeg.decode_jpeg( jpg_buffer, colorspace='BGR')
+    if out is None:
+        out = cv2.VideoWriter('outpy.mkv',cv2.VideoWriter_fourcc(*'XVID'), 24, (image.shape[1], image.shape[0]))
     out.write(image)
