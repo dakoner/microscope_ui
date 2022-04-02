5237ba73a5883762a0e8c84f5c76cc9c5f56e967
dek <dakoner@gmail.com>
2022-04-02 15:50:18 -0700

    make microscope ui object detection work.

--- microscope_ui.py
+++ microscope_ui.py
@@ -1,14 +1,20 @@
 import cv2
-#import cv2.aruco
 import time
 import sys
 import signal
+from PIL import Image, ImageDraw, ImageFont
 from PyQt5 import QtGui, QtCore, QtWidgets
+from six import BytesIO
 import paho.mqtt.client as mqtt
 import simplejpeg
 import imagezmq
 import numpy as np
-#from dlclive import DLCLive
+import time
+
+import tensorflow as tf
+from object_detection.utils import config_util
+# from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
 
 pcutoff=0.5
 pixel_to_mm = 0.0003
@@ -21,91 +27,94 @@ keys = (QtCore.Qt.Key_0, QtCore.Qt.Key_1, QtCore.Qt.Key_2, QtCore.Qt.Key_3, QtCo
 MovementKeys=(QtCore.Qt.Key_Left, QtCore.Qt.Key_Right, QtCore.Qt.Key_Up, QtCore.Qt.Key_Down, QtCore.Qt.Key_PageUp, QtCore.Qt.Key_PageDown)
 colormap = [QtCore.Qt.red, QtCore.Qt.green, QtCore.Qt.blue, QtCore.Qt.yellow, QtCore.Qt.magenta, QtCore.Qt.black]
 
+
+tardigrade_class_id = 1
+num_classes = 1
+category_index = {tardigrade_class_id: {'id': tardigrade_class_id, 'name': 'tardigrade'}}
+num_classes = 1
+pipeline_config = r'z:\src\tensorflow\models\research\object_detection\configs\tf2\ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.config'
+
+# Load pipeline config and build a detection model.
+#
+# Since we are working off of a COCO architecture which predicts 90
+# class slots by default, we override the `num_classes` field here to be just
+# one (for our new tardigrade class).
+configs = config_util.get_configs_from_pipeline_file(pipeline_config)
+model_config = configs['model']
+model_config.ssd.num_classes = num_classes
+model_config.ssd.freeze_batchnorm = True
+detection_model = model_builder.build(model_config=model_config, is_training=True)
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(r"z:\src\tardetect\tardigrade-1-1")
+
+counter = 0
+
+
+fontScale = 1
+color = (255, 0, 0)
+thickness = 2
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+class TFDetection(QtCore.QThread):
+    predictSignal = QtCore.pyqtSignal(float, QtCore.QRect)
+
+    def __init__(self, image):
+        super().__init__()
+        self.image = image
+
+    def run(self):
+        try:
+            s = self.image.shape
+            a= self.image.reshape((s[0], s[1], 3)).astype(np.uint8)
+            x = np.expand_dims(a, axis=0)
+            input_tensor = tf.convert_to_tensor(x, dtype=tf.float32)
+            preprocessed_image, shapes = detection_model.preprocess(input_tensor)
+            prediction_dict = detection_model.predict(preprocessed_image, shapes)
+            d= detection_model.postprocess(prediction_dict, shapes)
+            
+            score = d['detection_scores'][0].numpy()[0]
+            global counter
+            counter += 1
+            db = d['detection_boxes'][0].numpy()[0]
+            pt1 = int(db[1]*s[1]), int(db[0]*s[0])
+            pt2 =  int(db[3]*s[1]), int(db[2]*s[0])
+            r = QtCore.QRect(QtCore.QPoint(*pt1), QtCore.QPoint(*pt2))
+            self.predictSignal.emit(score, r)
+        except Exception as e:
+            print("Exception:", e)
+
 class ImageZMQCameraReader(QtCore.QThread):
-    signal = QtCore.pyqtSignal(np.ndarray, np.ndarray)
+    imageSignal = QtCore.pyqtSignal(np.ndarray)
+    predictSignal = QtCore.pyqtSignal(float, QtCore.QRect)
     def __init__(self):
         super(ImageZMQCameraReader, self).__init__()
         url = f"tcp://{IMAGEZMQ}:{PORT}"
         print("Connect to url", url)
         self.image_hub = imagezmq.ImageHub(url, REQ_REP=False)
-
-
-        # self.live = DLCLive(
-        #     model_path = r"C:\Users\dek\Desktop\tarditrack2-dek-2022-02-18\exported-models\DLC_tarditrack2_resnet_50_iteration-0_shuffle-1",
-        #     tf_config=None,
-        #     resize=0.5,
-        #     cropping=None,
-        #     dynamic=(False, 0.5, 10),
-        #     display=False,
-        #     pcutoff=0.5,
-        #     display_radius=3,
-        #     display_cmap='bmy',
-        # ) 
-
+        self.tf_detector = None
     def run(self):         
         name, jpg_buffer = self.image_hub.recv_jpg()
         image= simplejpeg.decode_jpeg( jpg_buffer, colorspace='RGB')
-        #poses = self.live.init_inference(image)
+        t0 = time.time()
 
-        #aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
-        #parameters =  cv2.aruco.DetectorParameters_create()
         while True:
             name, jpg_buffer = self.image_hub.recv_jpg()
-            image= simplejpeg.decode_jpeg( jpg_buffer, colorspace='RGB')
-            #corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
-            #cv2.aruco.drawDetectedMarkers(image, corners, ids)
-            
-            # gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-            # kernel_size = 25
-            # blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-            # low_threshold = 50
-            # high_threshold = 150
-            # edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-            # rho = 1  # distance resolution in pixels of the Hough grid
-            # theta = np.pi / 180  # angular resolution in radians of the Hough grid
-            # threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-            # min_line_length = 50  # minimum number of pixels making up a line
-            # max_line_gap = 20  # maximum gap in pixels between connectable line segments
-            # line_image = np.copy(image) * 0  # creating a blank to draw lines on
+            image_data = simplejpeg.decode_jpeg( jpg_buffer, colorspace='RGB')
+            t1 = time.time()
+            if t1 - t0 > 0.25:
+                if self.tf_detector and not self.tf_detector.isFinished():
+                    print("previous detector not finished!")
+                else:
+                    self.tf_detector = TFDetection(image_data.copy())
+                    self.tf_detector.predictSignal.connect(self.predict)
+                    self.tf_detector.start()
+                t0 = time.time()
 
-            # # Run Hough on edge detected image
-            # # Output "lines" is an array containing endpoints of detected line segments
-            # lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-            #                     min_line_length, max_line_gap)
-            # if lines is not None:
-            #     for line in lines:
-            #         for x1,y1,x2,y2 in line:
-            #             cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-            # lines_edges = cv2.addWeighted(image, 0.8, line_image, 1, 0)
+            self.imageSignal.emit(image_data)
 
-            #poses = self.live.get_pose(image)
-            #self.signal.emit(image, poses)
-
-            # img = cv2.medianBlur(image, 5)
-            # img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            # circles = cv2.HoughCircles(img,
-            #                            cv2.HOUGH_GRADIENT,
-            #                            dp=1,
-            #                            minDist=20,
-            #                            param1=50,
-            #                            param2=50,
-            #                            minRadius=20,
-            #                            maxRadius=800)
-
-            # if circles is not None:
-            #     circles = np.round(circles[0, :]).astype("int")
-            #     for (x, y, r) in circles[:1]:
-            #         DIAMETER=0.6 # mm
-            #         d = r*2
-            #         print("Diameter:", d, d/DIAMETER, x, y)
-            #         # At 4X mag, 0.6mm circle is ~512 pixels
-            #         # So ~850 pixels/mm or 0.00117 mm/pixel
-            #         # first positon:  580 356
-            #         # second position:  160 348 (after 0.5mm X move)
-            #         cv2.circle(image, (x, y), r, (0, 255, 0), 4)
-            #         cv2.rectangle(image, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-            self.signal.emit(image, np.zeros((5,3)))
-
+    def predict(self, score, box):
+        self.predictSignal.emit(score, box)
+        
 class Window(QtWidgets.QLabel):
 
     def __init__(self):
@@ -114,7 +123,9 @@ class Window(QtWidgets.QLabel):
         #self.resize(640,480)
         self.camera = ImageZMQCameraReader()
         self.camera.start()
-        self.camera.signal.connect(self.imageTo)
+        self.camera.imageSignal.connect(self.imageTo)
+        self.camera.predictSignal.connect(self.predictDraw)
+
 
         self.client =  mqtt.Client()
         self.client.on_connect = self.on_connect
@@ -134,6 +145,12 @@ class Window(QtWidgets.QLabel):
         self.tracking = False
         self.state = "Unknown"
 
+        self.prediction_score = None
+        self.prediction_box = None
+    
+    def predictDraw(self, score, box):
+        self.prediction_score = score
+        self.prediction_box = box
 
     def on_message(self, client, userdata, message):
         if message.topic == f"{TARGET}/m_pos":
@@ -161,7 +178,7 @@ class Window(QtWidgets.QLabel):
         cmd = "$J=G91 G0  X%.3f Y%.3f F%.3f"% (cursor_offset.y(), cursor_offset.x(), XY_FEED)
         self.client.publish(f"{TARGET}/command", cmd)
 
-    def imageTo(self, image, this_pose): 
+    def imageTo(self, image): 
         image = QtGui.QImage(image, image.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
         #(self.m_pos)
         
@@ -172,7 +189,7 @@ class Window(QtWidgets.QLabel):
             p.setCompositionMode( QtGui.QPainter.CompositionMode_SourceOver )
             p.setRenderHints( QtGui.QPainter.HighQualityAntialiasing )
 
-            p.drawImage(QtCore.QPoint(), image)
+            #p.drawImage(QtCore.QPoint(), image)
 
             pen = QtGui.QPen(QtCore.Qt.red)
             pen.setWidth(2)
@@ -184,33 +201,37 @@ class Window(QtWidgets.QLabel):
             font.setPointSize(24)
             p.setFont(font)
 
-            # c_pos = self.mapFromGlobal(QtGui.QCursor().pos())
-            # # Compute delta from c_pos to middle of window, then scale by pixel size
-            # s_pos = QtCore.QPoint(self.size().width()/2, self.size().height()/2)
-            p.drawText(750, 50, "X%8.3fmm" % self.m_pos[0])
-            p.drawText(750, 100, "Y%8.3fmm" % self.m_pos[1])
-            p.drawText(750, 150, "Z%8.3fmm" % self.m_pos[2])
-            # if self.tracking and this_pose[1,2] > pcutoff:
-            #     x = int(this_pose[1, 0])
-            #     y = int(this_pose[1, 1])
-            #     pos = QtCore.QPoint(x, y)
-            #     s_pos = QtCore.QPoint(self.size().width()/2, self.size().height()/2)
-            #     offset = QtCore.QPointF(pos-s_pos)*pixel_to_mm
-            #     cmd = "$J=G91 G0 F%.3f X%.3f Y%.3f"% (XY_FEED, offset.y(), offset.x())
-            #     t = time.time()
-            #     if self.time is None or t - self.time > 1.5:
-            #         self.client.publish(f"{TARGET}/command", cmd)
-            #         self.time = t
-
-            # for j in range(this_pose.shape[0]):
-            #     if this_pose[j, 2] > pcutoff:
-            #         x = int(this_pose[j, 0])
-            #         y = int(this_pose[j, 1])
-                    
-            #         p.setBrush(colormap[j])
-            #         p.setPen(QtGui.QPen(colormap[j]))   
-            #         p.drawEllipse(x, y, 5, 5 )
+            p.drawText(0, 50, "X%8.3fmm" % self.m_pos[0])
+            p.drawText(0, 100, "Y%8.3fmm" % self.m_pos[1])
+            p.drawText(0, 150, "Z%8.3fmm" % self.m_pos[2])
             p.end()
+
+        if self.prediction_score and self.prediction_score > 0.35:
+            p = QtGui.QPainter()
+        
+            p.begin(image)
+            p.setCompositionMode( QtGui.QPainter.CompositionMode_SourceOver )
+            p.setRenderHints( QtGui.QPainter.HighQualityAntialiasing )
+
+            pen = QtGui.QPen(QtCore.Qt.red)
+            pen.setWidth(2)
+            p.setPen(pen)  
+            p.drawRect(self.prediction_box)
+            
+            pen = QtGui.QPen(QtCore.Qt.green)
+            pen.setWidth(2)
+            p.setPen(pen)  
+            p.drawText(self.prediction_box.bottomRight(), "%5.2f" % self.prediction_score)
+            
+            image_center = QtCore.QPoint(image.width()/2, image.height()/2)
+            dt = self.prediction_box.center() - image_center
+            print(image_center, self.prediction_box.center(), dt)
+
+            p.drawEllipse(QtCore.QRect(image_center - QtCore.QPoint(2,2), image_center + QtCore.QPoint(2,2)))
+            p.drawLine(image_center, self.prediction_box.center())
+
+            p.end()
+
         pixmap = QtGui.QPixmap.fromImage(image)#.scaled(QtWidgets.QApplication.instance().primaryScreen().size(), QtCore.Qt.KeepAspectRatio)
         self.resize(pixmap.size().width(), pixmap.size().height())
         self.setPixmap(pixmap)
@@ -222,3 +243,4 @@ if __name__ == '__main__':
     window.show()#FullScreen()
 
     app.exec_()
+
