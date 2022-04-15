e8ac33d639bacdc90f7a611616added59596a7f3
dek <dakoner@gmail.com>
2022-04-15 00:12:00 +0000

    params and dimension order

--- image_stitch.py
+++ image_stitch.py
@@ -1,11 +1,11 @@
 import glob
 import os
-x_const = 500
-y_const = -500
+x_const = -3200
+y_const = -3500
 fnames = sorted(glob.glob("movie_grayscale/*.jpg"))
 files = [os.path.basename(fname)[:-4].split("_") for fname in fnames]
 a = [os.path.basename(fname) for fname in fnames]
-b = [(float(f[0])*x_const,float(f[1])*y_const) for f in files]
+b = [(float(f[1])*x_const,float(f[0])*y_const) for f in files]
 pto_vars = dict(zip(a, b))
 
 l = []
