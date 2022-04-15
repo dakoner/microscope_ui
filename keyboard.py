c7624979c5df90224d04689f0dd9f42cfdd495e1
dek <dakoner@gmail.com>
2022-04-15 00:11:03 +0000

    minor cleanups

--- keyboard.py
+++ keyboard.py
@@ -12,11 +12,11 @@ import sys
 STEP_SIZE=0.5
 TARGET=sys.argv[1]
 MQTT_SERVER="gork.local"
-XY_STEP_SIZE=500
-XY_FEED=100
+XY_STEP_SIZE=32
+XY_FEED=25
 
-Z_STEP_SIZE=0.05
-Z_FEED=50
+Z_STEP_SIZE=15
+Z_FEED=1
 
 class Driver:
     def __init__(self):
@@ -41,46 +41,51 @@ class Driver:
             for event in events:
                 if event.ev_type == 'Key':
                     if event.code == 'KEY_KPENTER' and event.state == 1:
+                        self.client.publish(f"{TARGET}/cancel")
                         self.client.publish(f"{TARGET}/pos", "push")
-                        continue
                     elif event.code == 'KEY_BACKSPACE' and event.state == 1:
+                        self.client.publish(f"{TARGET}/cancel")
                         self.client.publish(f"{TARGET}/pos", "pop")
-                        continue
                     elif event.code == 'KEY_KP0' and event.state == 1:
                         self.client.publish(f"{TARGET}/grid", "foo")
-                        continue
                     elif event.code == 'KEY_KP5' and event.state == 1:
                         print("Cancel")
                         self.client.publish(f"{TARGET}/cancel")
-                        continue          
+                    elif event.code == 'KEY_KP7' and event.state == 1:
+                        self.client.publish(f"{TARGET}/command", "M3S1000")
+                    elif event.code == 'KEY_KP9' and event.state == 1:
+                        self.client.publish(f"{TARGET}/command", "M5")
+                    elif event.code == 'KEY_KPASTERISK' and event.state == 1:
+                        self.client.publish(f"{TARGET}/track", "true")
+                    elif event.code == 'KEY_KPSLASH' and event.state == 1:
+                        self.client.publish(f"{TARGET}/track", "false")
                     elif event.code == 'KEY_KPDOT' and event.state == 1:
                         self.client.publish(f"{TARGET}/photo", "%s.jpg" % time.strftime("%Y%m%d-%H%M%S"))
-                        continue
                     elif event.code in ('KEY_KPMINUS', 'KEY_KPPLUS'):
                         if event.state == 0:
-                            continue          
+                            self.client.publish(f"{TARGET}/cancel")
                         elif event.state == 2:
-                            continue       
+                            pass       
                         else:
-                            if event.code == 'KEY_KPMINUS':
+                            self.client.publish(f"{TARGET}/cancel")
+                            if event.code == 'KEY_KPMINUS' and event.state == 1:
                                 cmd = f"$J=G91 G21 F{Z_FEED:.3f} Z-{Z_STEP_SIZE:.3f}"
                                 self.client.publish(f"{TARGET}/command", cmd)
                             elif event.code == 'KEY_KPPLUS' and event.state == 1:
                                 cmd = f"$J=G91 G21 F{Z_FEED:.3f} Z{Z_STEP_SIZE:.3f}"
-                                self.client.publish(f"{TARGET}/command", cmd)
+                                self.client.publish(f"{TARGET}/command", cmd)        
                     elif event.code in ('KEY_KP2', 'KEY_KP4', 'KEY_KP6', 'KEY_KP8'):
                         if event.state == 0:
-                            self.client.publish(f"{TARGET}/cancel")
-                            continue          
+                            self.client.publish(f"{TARGET}/cancel")          
                         elif event.state == 2:
-                            continue       
+                            pass       
                         else:
                             self.client.publish(f"{TARGET}/cancel")
                             if event.code == 'KEY_KP2' and event.state == 1:
-                                cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y{XY_STEP_SIZE:.3f}"
+                                cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y-{XY_STEP_SIZE:.3f}"
                                 self.client.publish(f"{TARGET}/command", cmd)
                             elif event.code == 'KEY_KP8' and event.state == 1:
-                                cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y-{XY_STEP_SIZE:.3f}"
+                                cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y{XY_STEP_SIZE:.3f}"
                                 self.client.publish(f"{TARGET}/command", cmd)
                             elif event.code == 'KEY_KP4' and event.state == 1:
                                 cmd = f"$J=G91  G21 F{XY_FEED:.3f} X{XY_STEP_SIZE:.3f}"
@@ -88,7 +93,8 @@ class Driver:
                             elif event.code == 'KEY_KP6' and event.state == 1:
                                 cmd = f"$J=G91 G21 F{XY_FEED:.3f} X-{XY_STEP_SIZE:.3f}"
                                 self.client.publish(f"{TARGET}/command", cmd)
-                            
+                    else:
+                        print(event.code, event.state)
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     d = Driver()
