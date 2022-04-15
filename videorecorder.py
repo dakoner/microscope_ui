f45eb40d029b22975b76f396bb51b0267706b2f1
dek <dakoner@gmail.com>
2022-04-15 00:11:20 +0000

    drop frame rate

--- videorecorder.py
+++ videorecorder.py
@@ -1,15 +1,18 @@
 import cv2
 import imagezmq
 import simplejpeg
-IMAGEZMQ='gork.local'
+IMAGEZMQ='gork'
 PORT=5556
 url = f"tcp://{IMAGEZMQ}:{PORT}"
 image_hub = imagezmq.ImageHub(url, REQ_REP=False)
 
 out = None
+i = 0
 while True:
     name, jpg_buffer = image_hub.recv_jpg()
     image= simplejpeg.decode_jpeg( jpg_buffer, colorspace='BGR')
     if out is None:
         out = cv2.VideoWriter('outpy.mkv',cv2.VideoWriter_fourcc(*'XVID'), 24, (image.shape[1], image.shape[0]))
-    out.write(image)
+    if i % 20 == 0:
+        out.write(image)
+    i += 1
