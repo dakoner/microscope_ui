fe091ad1f7a3acc68994e8274d1a7d47e30ef1cb
dek <dakoner@gmail.com>
2022-04-23 15:13:20 +0000

    cleanup and do edge/contour detection

--- microscope_ui.py
+++ microscope_ui.py
@@ -53,31 +53,38 @@ class ImageZMQCameraReader(QtCore.QThread):
             name, jpg_buffer = self.image_hub.recv_jpg()
             image_data = simplejpeg.decode_jpeg( jpg_buffer, colorspace='RGB')
                         
-            # gray = cv2.cvtColor(image_data,cv2.COLOR_RGB2GRAY)
-            # edges = cv2.Canny(gray,50,150,apertureSize = 3)
-            # #image_data = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
-
-            # lines = cv2.HoughLines(edges,1,np.pi/720,200)\
-            
-            # if lines is not None:
-            #     angles = []
-            #     for line in lines:
-            #         for rho,theta in line:
-            #             if (abs(theta) < 0.3):
-            #                 angles.append(theta)
-            #                 a = np.cos(theta)
-            #                 b = np.sin(theta)
-            #                 x0 = a*rho
-            #                 y0 = b*rho
-            #                 x1 = int(x0 + 1000*(-b))
-            #                 y1 = int(y0 + 1000*(a))
-            #                 x2 = int(x0 - 1000*(-b))
-            #                 y2 = int(y0 - 1000*(a))
-
-            #                 cv2.line(image_data,(x1,y1),(x2,y2),(0,0,255),2)
-            #     print(degrees(mean(angles)))
 
             self.imageSignal.emit(image_data)
+
+class ControlWindow(QtWidgets.QWidget):
+    def __init__(self, window):
+        super().__init__()
+        self.window = window
+
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.slider_one = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.slider_one.setMinimum(0)
+        self.slider_one.setMaximum(200)
+        self.slider_one.valueChanged[int].connect(self.slider_one_changed)
+
+        self.slider_two = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.slider_two.setMinimum(0)
+        self.slider_two.setMaximum(200)
+        self.slider_two.valueChanged[int].connect(self.slider_two_changed)
+
+        
+        layout.addWidget(self.slider_one)
+        layout.addWidget(self.slider_two)
+
+    def slider_one_changed(self, value):
+        print("one changed", value)
+        self.window.one = value
+
+    def slider_two_changed(self, value):
+        print("two changed", value)
+        self.window.two = value
 
 class Window(QtWidgets.QLabel):
 
@@ -108,7 +115,9 @@ class Window(QtWidgets.QLabel):
         self.state = "Unknown"
 
         self.results = None
-    
+        self.one = 0
+        self.two = 0
+
             
     def on_message(self, client, userdata, message):
         if message.topic == f"{TARGET}/m_pos":
@@ -135,9 +144,47 @@ class Window(QtWidgets.QLabel):
         cmd = "$J=G91  G21 X%.3f Y%.3f F%.3f"% (-cursor_offset.x(), -cursor_offset.y(), XY_FEED)
         self.client.publish(f"{TARGET}/command", cmd)
 
-    def imageTo(self, image): 
-        image = QtGui.QImage(image, image.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
-        #(self.m_pos)
+    def imageTo(self, image_data): 
+        # image_data = cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV)
+        # mask = cv2.inRange(image_data,
+        #                 np.array((0, 0, 0), dtype=np.uint8),
+        #                 np.array((255, 255, 5), dtype=np.uint8))
+        # mask = cv2.erode(mask, None, iterations=4)
+        # mask = cv2.dilate(mask, None, iterations=1)
+        
+        # image_data = cv2.cvtColor(image_data, cv2.COLOR_HSV2RGB)
+        # #blue = np.full_like(image_data, (255, 255, 0))
+        # #image_data = cv2.bitwise_and(blue, image_data, mask=mask)
+            
+
+        # # Find contours and find total area
+        # cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        # area = 0
+        # for c in cnts:
+        #     cv2.drawContours(image_data,[c], 0, (0,255,0), 2)
+            # lines = cv2.HoughLines(edges,1,np.pi/720,200)\
+            
+            # if lines is not None:
+            #     angles = []
+            #     for line in lines:
+            #         for rho,theta in line:
+            #             if (abs(theta) < 0.3):
+            #                 angles.append(theta)
+            #                 a = np.cos(theta)
+            #                 b = np.sin(theta)
+            #                 x0 = a*rho
+            #                 y0 = b*rho
+            #                 x1 = int(x0 + 1000*(-b))
+            #                 y1 = int(y0 + 1000*(a))
+            #                 x2 = int(x0 - 1000*(-b))
+            #                 y2 = int(y0 - 1000*(a))
+
+            #                 cv2.line(image_data,(x1,y1),(x2,y2),(0,0,255),2)
+            #     print(degrees(mean(angles)))
+
+
+        image = QtGui.QImage(image_data, image_data.shape[1], image_data.shape[0], QtGui.QImage.Format_RGB888)
         
         if self.m_pos is not None:
             p = QtGui.QPainter()
@@ -176,7 +223,7 @@ class Window(QtWidgets.QLabel):
             
             pen2 = QtGui.QPen(QtCore.Qt.blue)
             pen.setWidth(2)
-
+            first = True
             for result in self.results['results']:
                 prediction_score = result[0]
                 label = result[1]
@@ -187,9 +234,9 @@ class Window(QtWidgets.QLabel):
                 p.setPen(pen2)
                 p.drawText(box.bottomRight(), "%5.2f %s" % (prediction_score, label))
                 image_center = QtCore.QPointF(image.width()/2, image.height()/2)
-                if label == 'tardigrade' and prediction_score > 0.9:
+                if label == 'tardigrade' and prediction_score == 1.0 and first:
                     p.drawLine(image_center, box.center())
-                   
+                    first = False
 
             self.results = None
             p.end()
@@ -202,7 +249,9 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
+    cw = ControlWindow(window)
     window.show()#FullScreen()
+    cw.show()
 
     app.exec_()
 
