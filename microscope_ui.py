1cb2369fd65d23a4f7d1604197332df36b0519f5
dek <dakoner@gmail.com>
2022-05-14 16:17:25 -0700

    Experimenting with more line detection.

--- microscope_ui.py
+++ microscope_ui.py
@@ -1,4 +1,4 @@
-from math import degrees
+from math import degrees, cos, sin
 from statistics import mean
 from calendar import c
 import os
@@ -16,6 +16,8 @@ import numpy as np
 import time
 import classes
 import json
+from collections import deque
+
 pcutoff=0.5
 pixel_to_mm = 0.00005
 MQTT_SERVER="dekscope.local"
@@ -92,23 +94,43 @@ class ControlWindow(QtWidgets.QWidget):
         
         self.slider_threshold = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
         self.slider_threshold.setMinimum(0)
-        self.slider_threshold.setMaximum(200)
+        self.slider_threshold.setMaximum(500)
         self.slider_threshold.valueChanged[int].connect(self.slider_threshold_changed)
         self.slider_threshold.setValue(self.window.threshold)
     
         self.slider_minLineLength = QtWidgets.QDoubleSpinBox(self)
         self.slider_minLineLength.setMinimum(0)
-        self.slider_minLineLength.setMaximum(100)
+        self.slider_minLineLength.setMaximum(500)
         self.slider_minLineLength.setSingleStep(1)
         self.slider_minLineLength.valueChanged[float].connect(self.slider_minLineLength_changed)
         self.slider_minLineLength.setValue(self.window.minLineLength)
         
         self.slider_maxLineGap = QtWidgets.QDoubleSpinBox(self)
         self.slider_maxLineGap.setMinimum(0)
-        self.slider_maxLineGap.setMaximum(100)
+        self.slider_maxLineGap.setMaximum(500)
         self.slider_maxLineGap.setSingleStep(1)
         self.slider_maxLineGap.valueChanged[float].connect(self.slider_maxLineGap_changed)
         self.slider_maxLineGap.setValue(self.window.maxLineGap)
+
+
+        self.slider_mask_thresh = QtWidgets.QDoubleSpinBox(self)
+        self.slider_mask_thresh.setMinimum(0)
+        self.slider_mask_thresh.setMaximum(500)
+        self.slider_mask_thresh.setSingleStep(1)
+        self.slider_mask_thresh.valueChanged[float].connect(self.slider_mask_thresh_changed)
+        self.slider_mask_thresh.setValue(self.window.mask_thresh)
+        
+        self.slider_erode_iterations = QtWidgets.QSpinBox(self)
+        self.slider_erode_iterations.setMinimum(0)
+        self.slider_erode_iterations.setMaximum(5)
+        self.slider_erode_iterations.valueChanged[int].connect(self.slider_erode_iterations_changed)
+        self.slider_erode_iterations.setValue(self.window.erode_iterations)
+
+        self.slider_dilate_iterations = QtWidgets.QSpinBox(self)
+        self.slider_dilate_iterations.setMinimum(0)
+        self.slider_dilate_iterations.setMaximum(5)
+        self.slider_dilate_iterations.valueChanged[int].connect(self.slider_dilate_iterations_changed)
+        self.slider_dilate_iterations.setValue(self.window.erode_iterations)
 
         layout.addWidget(self.slider_one)
         layout.addWidget(self.slider_two)
@@ -116,6 +138,9 @@ class ControlWindow(QtWidgets.QWidget):
         layout.addWidget(self.slider_threshold)
         layout.addWidget(self.slider_minLineLength)
         layout.addWidget(self.slider_maxLineGap)
+        layout.addWidget(self.slider_mask_thresh)
+        layout.addWidget(self.slider_erode_iterations)
+        layout.addWidget(self.slider_dilate_iterations)
 
     def slider_one_changed(self, value):
         print("one changed", value)
@@ -140,6 +165,18 @@ class ControlWindow(QtWidgets.QWidget):
     def slider_maxLineGap_changed(self, value):
         print("maxLineGap changed", value)
         self.window.maxLineGap = value
+
+    def slider_mask_thresh_changed(self, value):
+        print("mask_thresh changed", value)
+        self.window.mask_thresh = value
+
+    def slider_erode_iterations_changed(self, value):
+        print("erode_iterations changed", value)
+        self.window.erode_iterations = value
+
+    def slider_dilate_iterations_changed(self, value):
+        print("dilate_iterations changed", value)
+        self.window.dilate_iterations = value
 
 class Window(QtWidgets.QLabel):
 
@@ -174,12 +211,18 @@ class Window(QtWidgets.QLabel):
         self.two = 100
 
         self.rho = 1 # double
-        self.theta = np.pi/2 # double
-        self.threshold = 1 # int
-        self.minLineLength = 25 # double
-        self.maxLineGap = 25 # double
+        self.theta = np.pi/50 # double
+        self.threshold = 15 # int
+        self.minLineLength = 100 # double
+        self.maxLineGap = 100 # double
+
+        self.mask_thresh = 100
+        self.erode_iterations = 1
+        self.dilate_iterations = 1
 
         self.old_image = None 
+
+        self.queue = deque()
 
     def on_message(self, client, userdata, message):
         if message.topic == f"{TARGET}/m_pos":
@@ -207,12 +250,13 @@ class Window(QtWidgets.QLabel):
         self.client.publish(f"{TARGET}/command", cmd)
 
     def get_mask(self, image_data):
-        image_data = cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV)
+        image_data = cv2.cvtColor(image_data, cv2.COLOR_BGR2HSV)
+        image_data = cv2.GaussianBlur(image_data, (3, 3), 1)
         mask = cv2.inRange(image_data,
                         np.array((0, 0, 0), dtype=np.uint8),
-                        np.array((255, 255, 5), dtype=np.uint8))
-        mask = cv2.erode(mask, None, iterations=4)
-        mask = cv2.dilate(mask, None, iterations=1)
+                        np.array((255, 255, self.mask_thresh), dtype=np.uint8))
+        mask = cv2.dilate(mask, None, iterations=self.dilate_iterations)
+        mask = cv2.erode(mask, None, iterations=self.erode_iterations)
         
         return mask
         
@@ -222,34 +266,39 @@ class Window(QtWidgets.QLabel):
         cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
         area = 0
-        gray = cv2.cvtColor(image_data,cv2.COLOR_BGR2GRAY)
+        #gray = cv2.cvtColor(image_data,cv2.COLOR_BGR2GRAY)
 
         for c in cnts:
-            cv2.drawContours(gray,[c], 0, (0,255,0), 2)
-        return cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+            cv2.drawContours(image_data,[c], 0, (255,255,255), 2)
+        return image_data
+        #return cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
     def find_lines(self, image_data):
-        src = cv2.GaussianBlur(image_data, (3, 3), 0)
-        gray = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray,self.one, self.two, apertureSize = 3)
-        #kernel = np.ones((3,3), np.uint8)
-        #img_dilation = cv2.dilate(edges, kernel, iterations=1)
-        #img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
+        gray = cv2.cvtColor(image_data,cv2.COLOR_BGR2GRAY)
+        #edges = cv2.Canny(gray,self.one, self.two, apertureSize = 3)
+        #ret, thresh1 = cv2.threshold(gray,25, 255, cv2.THRESH_BINARY)
+        #src = cv2.GaussianBlur(thresh1, (3, 3), 0)
+        #kernel = np.ones((4,4), np.uint8)
+        #img_erosion = cv2.erode(edges, kernel, iterations=1)
+        #img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+        #lines_src = cv2.bitwise_not(img_erosion)
+        #lines_src = cv2.GaussianBlur(lines_src, (3, 3), 0)
+        lines = cv2.HoughLinesP(gray, self.rho, self.theta, self.threshold, self.minLineLength, self.maxLineGap)
 
-        lines = cv2.HoughLinesP(edges,self.rho, self.theta,self.threshold, self.minLineLength, self.maxLineGap)
-        draw_data = image_data.copy()
+        draw_data = np.zeros_like(image_data) #image_data #cv2.cvtColor(image_data, cv2.COLOR_GRAY2BGR)
+        print(draw_data.shape, draw_data.dtype)
         if lines is not None:
-            for line in lines:
-                for x1, y1, x2, y2 in line:
-                    cv2.line(draw_data,(x1,y1),(x2,y2),(0,255,255),2)
-        return draw_data #cv2.cvtColor(draw_data, cv2.COLOR_GRAY2BGR)
+           for line in lines:
+               for x1, y1, x2, y2 in line:
+                   cv2.line(draw_data,(x1,y1),(x2,y2),(0,255,255),1)
+        return draw_data
     
     def sobel(self, image_data):
         scale = 1
         delta = 0
         ddepth = cv2.CV_16S
 
-        src = cv2.GaussianBlur(image_data, (3,3), 0)
+        src = cv2.GaussianBlur(image_data, (3,3), 1)
         gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
         grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
         grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
@@ -263,32 +312,39 @@ class Window(QtWidgets.QLabel):
     
 
     def imageTo(self, image_data): 
+        # implement queue (via list) 
+        if len(self.queue) == 10:
+           self.queue.popleft()
+        self.queue.append(image_data)
 
-        if self.old_image is None:
-            self.old_image = image_data
-            return
-
-
+        #avg_image = np.mean(self.queue, axis=(0,)).astype(np.uint8)
 
         #image_data = cv2.addWeighted(self.old_image, 0.9, image_data,  0.1, 0)
 
 
-        #image_data = self.sobel(image_data)
+        #draw_data = self.sobel(image_data)
+        mask = self.get_mask(image_data)
+        image_data = cv2.bitwise_and(image_data, image_data, mask = mask)
+        print(image_data.shape)
         draw_data = self.find_lines(image_data)
         #draw_data = self.find_contours(image_data, mask)
 
+        #draw_data = avg_image
+        #mask = self.get_mask(image_data)
+        #draw_data = cv2.bitwise_and(image_data, image_data, mask = mask)
+        #draw_data = res
+        #draw_data = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
+        #mask = self.get_mask(image_data)
+        #image_data = cv2.cvtColor(image_data, cv2.COLOR_HSV2RGB)
+        #blue = np.full_like(image_data, (0, 0, 255))
+        #draw_data = cv2.bitwise_and(blue, image_data, mask=mask)
 
-
-        # mask = self.get_mask(image_data)
-        # image_data = cv2.cvtColor(image_data, cv2.COLOR_HSV2RGB)
-        # blue = np.full_like(image_data, (255, 0, 0))
-        # draw_data = cv2.bitwise_and(blue, image_data, mask=mask)
-
-        #self.find_contours(image_data, mask)
-        #draw_data = self.find_lines(image_data)
+        #self.find_contours(avg_image, mask)
+        #draw_data = avg_image
+        #draw_data = self.find_lines(mask)
         #draw_data = image_data
-        self.old_image = image_data
+
         image = QtGui.QImage(draw_data, draw_data.shape[1], draw_data.shape[0], QtGui.QImage.Format_RGB888)
 
         if self.m_pos is not None:
