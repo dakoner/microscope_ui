f4526020ce818dee7c0053c9b8160bfd8d948b59
dek <dakoner@gmail.com>
2022-08-19 15:49:31 -0700

    disable autorepeat

--- microscope_ui.py
+++ microscope_ui.py
@@ -33,7 +33,7 @@ MQTT_SERVER="192.168.1.75"
 IMAGEZMQ='192.168.1.75'
 TARGET=sys.argv[1]
 PORT=sys.argv[2]
-XY_STEP_SIZE=32
+XY_STEP_SIZE=1
 XY_FEED=50
 
 Z_STEP_SIZE=15
@@ -96,58 +96,59 @@ class MplWindow(QtWidgets.QWidget):
 class Window(QtWidgets.QLabel):
     def eventFilter(self, widget, event):
         if isinstance(event, QtGui.QKeyEvent):
-            print(event, event.type())
-            key = event.key()    
+            print(event, event.type(), event.isAutoRepeat())
+            if not event.isAutoRepeat():
+                key = event.key()    
 
-            if key == QtCore.Qt.Key_Left:
-                print("left")
-                if event.type() == QtCore.QEvent.KeyRelease:
-                    self.client.publish(f"{TARGET}/cancel")
-                elif event.type() == QtCore.QEvent.KeyPress:
-                    self.client.publish(f"{TARGET}/cancel")
-                    cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y-{XY_STEP_SIZE:.3f}"
-                    self.client.publish(f"{TARGET}/command", cmd)
-            elif key == QtCore.Qt.Key_Right:
-                print("right")
-                if event.type() == QtCore.QEvent.KeyRelease:
-                    self.client.publish(f"{TARGET}/cancel")
-                elif event.type() == QtCore.QEvent.KeyPress:
-                    self.client.publish(f"{TARGET}/cancel")
-                    cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y{XY_STEP_SIZE:.3f}"
-                    self.client.publish(f"{TARGET}/command", cmd)
-            elif key == QtCore.Qt.Key_Up:
-                print("up")
-                if event.type() == QtCore.QEvent.KeyRelease:
-                    self.client.publish(f"{TARGET}/cancel")
-                elif event.type() == QtCore.QEvent.KeyPress:
-                    self.client.publish(f"{TARGET}/cancel")
-                    cmd = f"$J=G91 G21 F{XY_FEED:.3f} X-{XY_STEP_SIZE:.3f}"
-                    self.client.publish(f"{TARGET}/command", cmd)
-            elif key == QtCore.Qt.Key_Down:
-                print("down")
-                if event.type() == QtCore.QEvent.KeyRelease:
-                    self.client.publish(f"{TARGET}/cancel")
-                elif event.type() == QtCore.QEvent.KeyPress:
-                    self.client.publish(f"{TARGET}/cancel")
-                    cmd = f"$J=G91 G21 F{XY_FEED:.3f} X{XY_STEP_SIZE:.3f}"
-                    self.client.publish(f"{TARGET}/command", cmd)
-            elif key == QtCore.Qt.Key_Plus:
-                print("plus")
-                if event.type() == QtCore.QEvent.KeyRelease:
-                    self.client.publish(f"{TARGET}/cancel")
-                elif event.type() == QtCore.QEvent.KeyPress:
-                    self.client.publish(f"{TARGET}/cancel")
-                    cmd = f"$J=G91 G21 F{Z_FEED:.3f} Z-{Z_STEP_SIZE:.3f}"
-                    self.client.publish(f"{TARGET}/command", cmd)
-            elif key == QtCore.Qt.Key_Minus:
-                print("plus")
-                if event.type() == QtCore.QEvent.KeyRelease:
-                    self.client.publish(f"{TARGET}/cancel")
-                elif event.type() == QtCore.QEvent.KeyPress:
-                    self.client.publish(f"{TARGET}/cancel")
-                    cmd = f"$J=G91 G21 F{Z_FEED:.3f} Z{Z_STEP_SIZE:.3f}"
-                    self.client.publish(f"{TARGET}/command", cmd)
-                       
+                if key == QtCore.Qt.Key_Left:
+                    print("left")
+                    if event.type() == QtCore.QEvent.KeyRelease:
+                        self.client.publish(f"{TARGET}/cancel")
+                    elif event.type() == QtCore.QEvent.KeyPress:
+                        self.client.publish(f"{TARGET}/cancel")
+                        cmd = f"$J=G91 G21 F{XY_FEED:.3f} X-{XY_STEP_SIZE:.3f}"
+                        self.client.publish(f"{TARGET}/command", cmd)
+                elif key == QtCore.Qt.Key_Right:
+                    print("right")
+                    if event.type() == QtCore.QEvent.KeyRelease:
+                        self.client.publish(f"{TARGET}/cancel")
+                    elif event.type() == QtCore.QEvent.KeyPress:
+                        self.client.publish(f"{TARGET}/cancel")
+                        cmd = f"$J=G91 G21 F{XY_FEED:.3f} X{XY_STEP_SIZE:.3f}"
+                        self.client.publish(f"{TARGET}/command", cmd)
+                elif key == QtCore.Qt.Key_Up:
+                    print("up")
+                    if event.type() == QtCore.QEvent.KeyRelease:
+                        self.client.publish(f"{TARGET}/cancel")
+                    elif event.type() == QtCore.QEvent.KeyPress:
+                        self.client.publish(f"{TARGET}/cancel")
+                        cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y-{XY_STEP_SIZE:.3f}"
+                        self.client.publish(f"{TARGET}/command", cmd)
+                elif key == QtCore.Qt.Key_Down:
+                    print("down")
+                    if event.type() == QtCore.QEvent.KeyRelease:
+                        self.client.publish(f"{TARGET}/cancel")
+                    elif event.type() == QtCore.QEvent.KeyPress:
+                        self.client.publish(f"{TARGET}/cancel")
+                        cmd = f"$J=G91 G21 F{XY_FEED:.3f} Y{XY_STEP_SIZE:.3f}"
+                        self.client.publish(f"{TARGET}/command", cmd)
+                elif key == QtCore.Qt.Key_Plus:
+                    print("plus")
+                    if event.type() == QtCore.QEvent.KeyRelease:
+                        self.client.publish(f"{TARGET}/cancel")
+                    elif event.type() == QtCore.QEvent.KeyPress:
+                        self.client.publish(f"{TARGET}/cancel")
+                        cmd = f"$J=G91 G21 F{Z_FEED:.3f} Z-{Z_STEP_SIZE:.3f}"
+                        self.client.publish(f"{TARGET}/command", cmd)
+                elif key == QtCore.Qt.Key_Minus:
+                    print("plus")
+                    if event.type() == QtCore.QEvent.KeyRelease:
+                        self.client.publish(f"{TARGET}/cancel")
+                    elif event.type() == QtCore.QEvent.KeyPress:
+                        self.client.publish(f"{TARGET}/cancel")
+                        cmd = f"$J=G91 G21 F{Z_FEED:.3f} Z{Z_STEP_SIZE:.3f}"
+                        self.client.publish(f"{TARGET}/command", cmd)
+                        
 
         return super().eventFilter(widget, event)
 
@@ -189,22 +190,33 @@ class Window(QtWidgets.QLabel):
 
         self.queue = deque()
 
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.timerExpired)
+        self.timer.start(1000)
+
+    def timerExpired(self, *args):
+        print("timer:", args)
+        if self.connected:
+            self.client.publish(f"{TARGET}/command", "?")
+
     def on_message(self, client, userdata, message):
         if message.topic == f"{TARGET}/m_pos":
             self.m_pos = eval(message.payload)
         # elif message.topic == f"{TARGET}/inference":
         #     self.results = json.loads(message.payload)
+        elif message.topic == f"{TARGET}/state":
+            self.state = message.payload.decode('utf-8')
 
     def on_connect(self, client, userdata, flags, rc):
         print("connected")
         self.connected = True
         self.client.subscribe(f"{TARGET}/m_pos")
-        self.client.subscribe(f"{TARGET}/inference")
+        #self.client.subscribe(f"{TARGET}/inference")
+        self.client.subscribe(f"{TARGET}/state")
 
     def on_disconnect(self, client, userdata, flags):
         print("disconnected")
         self.connected = False
-        #self.timer.stop()
 
 
     def mousePressEvent(self, event):
@@ -241,41 +253,13 @@ class Window(QtWidgets.QLabel):
             font.setPointSize(24)
             p.setFont(font)
 
-            p.drawText(0, 50, "X%8.3fmm" % self.m_pos[0])
-            p.drawText(0, 100, "Y%8.3fmm" % self.m_pos[1])
-            p.drawText(0, 150, "Z%8.3fmm" % self.m_pos[2])
+            p.drawText(0, 50, self.state)
+            p.drawText(0, 100, "X%8.3fmm" % self.m_pos[0])
+            p.drawText(0, 150, "Y%8.3fmm" % self.m_pos[1])
+            p.drawText(0, 200, "Z%8.3fmm" % self.m_pos[2])
             p.end()
 
-        # if self.results:
-        #     p = QtGui.QPainter()
-        
-        #     p.begin(image)
-        #     p.setCompositionMode( QtGui.QPainter.CompositionMode_SourceOver )
-        #     p.setRenderHints( QtGui.QPainter.HighQualityAntialiasing )
 
-        #     pen1 = QtGui.QPen(QtCore.Qt.black)
-        #     pen1.setWidth(2)
-                
-            
-        #     pen2 = QtGui.QPen(QtCore.Qt.blue)
-        #     pen2.setWidth(2)
-        #     first = True
-        #     for result in self.results['results']:
-        #         prediction_score = result[0]
-        #         label = result[1]
-        #         box = result[2]
-        #         p.setPen(pen1)
-        #         box = QtCore.QRectF(QtCore.QPointF(*box[0]), QtCore.QPointF(*box[1]))
-        #         p.drawRect(box)
-        #         p.setPen(pen2)
-        #         p.drawText(box.bottomRight(), "%5.2f %s" % (prediction_score, label))
-        #         image_center = QtCore.QPointF(image.width()/2, image.height()/2)
-        #         if label == 'tardigrade' and prediction_score == 1.0 and first:
-        #             p.drawLine(image_center, box.center())
-        #             first = False
-
-        #     self.results = None
-        #     p.end()
 
         pixmap = QtGui.QPixmap.fromImage(image)#.scaled(QtWidgets.QApplication.instance().primaryScreen().size(), QtCore.Qt.KeepAspectRatio)
         self.resize(pixmap.size().width(), pixmap.size().height())
