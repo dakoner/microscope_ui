0d9eb6aa83c65f87fc05ffde09a1be85aba6d586
dek <dakoner@gmail.com>
2022-08-27 21:35:17 +0000

    elide pyspin, change settings for latency.

--- imagezmq_sender.py
+++ imagezmq_sender.py
@@ -7,32 +7,16 @@ import numpy as np
 HOST='dekscope.local'
 #port = 5555
 
-# if __name__ == '__main__':
-#    from simple_pyspin import Camera
-#    cam = Camera()
-#    cam.init()
-#    sender = imagezmq.ImageSender("tcp://*:{}".format(port), REQ_REP=False)
-#    cam.start()
-#    while True:
-#        img = cam.get_array()
-#        # Convert to 3 dimensions
-#        img = img[..., np.newaxis]
-#        # Repeat inner element to turn 8-bit grayscale into 8-bit RGB.
-       
-#        img = np.repeat(img, repeats=3, axis=2)
-#        jpg_buffer = simplejpeg.encode_jpeg(img, quality=55, colorspace='RGB')
-#        sender.send_jpg("inspectionscope", jpg_buffer)
-
+#width = 3264;height = 2448
+#width = 640; height = 480
+#width=640; height=480
+#width=1280;height=720
+width=800;height=600
 
 if __name__ == '__main__':
     import cv2
     cap = cv2.VideoCapture(int(sys.argv[1]))
     port = int(sys.argv[2])
-    #width = 3264;height = 2448
-    #width = 640; height = 480
-    #width=640; height=480
-    width=1600;height=1200
-    #width=1280;height=720
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     sender = imagezmq.ImageSender("tcp://*:{}".format(port), REQ_REP=False)
@@ -45,12 +29,7 @@ if __name__ == '__main__':
             t1 = time.time()
             if t1 - t0 >= 0.1:
             #if counter % 5 == 0:
-                #b, g, r = cv2.split(img)
-                #result = g
-                #result = (r+b)/(r-b+0.0001)*256
-                #print(result)
-                #img = cv2.merge([result, result, result]).astype(np.uint8)
-                jpg_buffer = simplejpeg.encode_jpeg(img, quality=98, colorspace='BGR')
+                jpg_buffer = simplejpeg.encode_jpeg(img, quality=50, colorspace='BGR')
                 sender.send_jpg(HOST, jpg_buffer)
                 t0 = t1
         counter += 1
