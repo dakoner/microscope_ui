4c8bb5fa4e9769c0aafc0d496b55e4d2bcf86e16
David Konerding <dakoner@gmail.com>
2022-11-09 15:05:17 -0800

    Make stitcher UI (work in progress)

--- stitcher.py
+++ stitcher.py
@@ -1,9 +1,11 @@
+import glob
 import json
 import sys
 sys.path.append("..")
 import signal
 from PyQt5 import QtGui, QtCore, QtWidgets
 import os
+from PyQt5.uic import loadUi
 
 
 def parse(filename):
@@ -28,7 +30,6 @@ def parse(filename):
                 fname = os.path.join(dir_, image_filename)
                
                 results.append((fname, eval(coords)))
-    print(len(results))
     return results
 
 
@@ -36,8 +37,8 @@ class Scene(QtWidgets.QGraphicsScene):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        results = parse("z:\\src\\microscope_ui\\movie\\TileConfiguration.txt")
 
+    def addResults(self, results):
         for filename, coords in results:
             if not os.path.exists(filename):
                 print("Could not locate", filename)
@@ -47,25 +48,130 @@ class Scene(QtWidgets.QGraphicsScene):
                 item = self.addPixmap(p)
                 item.setPos(*coords)
 
-
 class TileWindow(QtWidgets.QGraphicsView):
    
-    def __init__(self, *args, **kwargs):
+    def __init__(self, results, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.scene = Scene()
+        self.scene.addResults(results)
+
         self.setScene(self.scene)
-        
-        self.fitInView(self.scene.sceneRect())
+
+        self.setMouseTracking(True)
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+
+
+        r = self.scene.sceneRect()
+        self.fitInView(r, QtCore.Qt.KeepAspectRatio)
+        self.scale(2,2)
+
+    def mousePressEvent(self, event):
+        print("mouse press", event.buttons())
+        self.press = event.pos()
+
+    def mouseReleaseEvent(self, event):
+        print("mouse release", event.buttons())
+
+        # if event.buttons() & QtCore.Qt.LeftButton:
+        #     delta = event.pos() - self.press
+        #     print("delta", delta)
+        #     self.translate(delta.x(), delta.y())
+        # elif event.buttons() & QtCore.Qt.RightButton:
+        #     delta = event.pos() - self.press
+        #     if delta.y() > -10:
+        #         scale = 1.01**delta.y()
+        #         print("delta", delta.y(), "scale", scale)
+        #         self.scale(scale, scale)
+        self.press = None
+
+    def mouseMoveEvent(self, event):
+        # if event.buttons() & QtCore.Qt.RightButton:
+        #     delta = event.pos() - self.press
+        #     if delta.y() > -10:
+        #         scale = 1.01**delta.y()
+        #         print("delta", delta.y(), "scale", scale)
+        #         self.scale(scale, scale)
+        if event.buttons() & QtCore.Qt.LeftButton:
+            print("mouse moved while left button pressed")
+            delta = event.pos() - self.press
+            print("delta", delta)
+            #self.translate(delta.x(), delta.y())
+            self.translate(1, 1)
+            #event.accept()
+
+
+    def resizeEvent(self, event):
+        print("tile window resizeEvent")
+        #self.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+        #self.scale(2,2)
+        event.accept()
+
+
+class TabWidget(QtWidgets.QTabWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print("custom tab widget")
+
+
+    def resizeEvent(self, event):
+        print("tab widget resizeEvent")
+
+
+
+class CentralWidget(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print("central widget")
+        print("findChild", self.findChild(QtWidgets.QTabWidget))
+
+
+    def resizeEvent(self, event):
+        print("central widget resizeEvent")
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        loadUi("stitcher.ui", self)
+        print(self.dumpObjectTree())
+        #self.run()
+        self.setCentralWidget(self.centralwidget)
+        self.run()
+
+    def run(self):
+        g = glob.glob(sys.argv[1])
+        g.sort(key=lambda x: int(os.path.basename(x)))
+        for f in g[:1]:
+            results = parse(f"{f}\\TileConfiguration.txt")
+            #print(results)
+            tile_window = TileWindow(results)
+            label = os.path.basename(f)
+            self.tabWidget.addTab(tile_window, label)
+            self.tabWidget.show()
+
+
+    def resizeEvent(self, event):
+        print("main window  resize event")
+        self.centralWidget().resizeEvent(event)
 
 class QApplication(QtWidgets.QApplication):
     def __init__(self, *argv):
         super().__init__(*argv)
 
-        self.tile_window = TileWindow()
-        self.tile_window.show()
+
+        self.main_window = MainWindow()
+        self.main_window.show()
+
+
+
 
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
     app = QApplication(sys.argv)
     app.exec_()
