255a481a0109b99109279aeb614596eefda9db35
David Konerding <dakoner@gmail.com>
2022-11-20 15:51:07 -0800

    simple fixes

--- stitcher/test_simplestitch_chimerax.py
+++ stitcher/test_simplestitch_chimerax.py
@@ -12,6 +12,7 @@ def main():
     g = glob.glob("movie/*")
     g.sort()
     prefix = g[-1]
+    print(prefix)
     d=json.load(open(f"{prefix}/scan_config.json"))
     r=pd.read_json(f"{prefix}/tile_config.json", lines=True)
     r.set_index(['counter', 'i', 'j', 'k'])
@@ -20,19 +21,20 @@ def main():
     x_max = d['k_dim']*FOV_X_PIXELS+WIDTH
     y_max = d['j_dim']*FOV_Y_PIXELS+HEIGHT
     z_max = d['i_dim']
-    for t in r.counter.unique()[:-1]:
+    for t in r.counter.unique()[:1]:
         print(t)
         # Get all items in time t
         all_t = r[r.counter == t]
 
         # We now have all the CZYX data for a time    
         for i in range(d['i_dim']):
+            print("\t", i, d['i_dim'][:-1])
             all_ti = all_t[all_t.i == i]
             
             o = np.zeros(shape=(3, y_max, x_max), dtype=np.ubyte)
             for row in all_ti.itertuples():
                 fname = row.fname
-                data = tifffile.imread(row.fname)
+                data = tifffile.imread(os.path.join(prefix, row.fname))
                 x0 = row.k * FOV_X_PIXELS
                 y0 = row.j * FOV_Y_PIXELS
                 x1 = x0 + WIDTH
