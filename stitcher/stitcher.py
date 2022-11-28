5e071218b92f194a56909041a910a2db57ecfcd9
David Konerding <dakoner@gmail.com>
2022-11-28 14:53:06 -0800

    Improve stitcher

--- stitcher/stitcher.py
+++ stitcher/stitcher.py
@@ -12,106 +12,86 @@ from PyQt5.uic import loadUi
 from microscope_ui.config import FOV_X_PIXELS, FOV_Y_PIXELS, WIDTH, HEIGHT # should obtain from movie json.
 
 
-
-class Scene(QtWidgets.QGraphicsScene):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-
-    def addResults(self, prefix, d):
-        counter = 0
-        for row in d.itertuples():
-            fname = os.path.join(prefix, row.fname)
-            if row.k == 0:
-                continue
-            x0 = row.k * FOV_X_PIXELS
-            y0 = row.j * FOV_Y_PIXELS
-            image = QtGui.QImage(fname)
-            pixmap = QtGui.QPixmap(image)
-            item = self.addPixmap(pixmap)
-            item.setPos(x0, y0)
-
-
-    # def resizeEvent(self, event):
-    #     print("scene resize")
-
 class TileWindow(QtWidgets.QGraphicsView):
    
-    def __init__(self, prefix, results, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.scene = Scene()
-        self.scene.addResults(prefix, results)
-        self.setScene(self.scene)
-
-        self.setMouseTracking(True)
-        #self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
-        #self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
-        #self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        #self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-
-
-        self.scene.setSceneRect(self.scene.itemsBoundingRect())
-        self.setSceneRect(self.scene.sceneRect())
-        #r = self.scene.itemsBoundingRect()
-        self.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
 
         self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
 
     def keyPressEvent(self, event):
-        print("tile graphics view key press event")
         key = event.key()  
         if key == QtCore.Qt.Key_Plus:
-            self.scale(2, 2)
+            self.scale(1.1, 1.1)
+            event.accept()
         elif key == QtCore.Qt.Key_Minus:
-            self.scale(0.5, 0.5)
+            self.scale(0.9,0.9)
+            event.accept()
         return super().keyPressEvent(event)
 
-    # def mousePressEvent(self, event):
-    #     print("mouse press", event.buttons())
-    #     self.press = event.pos()
+    def mousePressEvent(self, event):
+        print("mouse press", event.buttons())
+        if event.buttons() & QtCore.Qt.RightButton:
+            self.press = event.pos()
 
-    # def mouseReleaseEvent(self, event):
-    #     print("mouse release", event.buttons())
+    def mouseReleaseEvent(self, event):
+        print("mouse release", event.buttons())
+        if event.buttons() & QtCore.Qt.RightButton:
+            delta = event.pos() - self.press
+            if delta.y() > -10:
+                scale = 1.01**delta.y()
+                print("delta", delta.y(), "scale", scale)
+                self.scale(scale, scale)
+        self.press = None
 
-        # if event.buttons() & QtCore.Qt.LeftButton:
-        #     delta = event.pos() - self.press
-        #     print("delta", delta)
-        #     self.translate(delta.x(), delta.y())
-        # elif event.buttons() & QtCore.Qt.RightButton:
-        #     delta = event.pos() - self.press
-        #     if delta.y() > -10:
-        #         scale = 1.01**delta.y()
-        #         print("delta", delta.y(), "scale", scale)
-        #         self.scale(scale, scale)
-    #    self.press = None
-
-    # def mouseMoveEvent(self, event):
-    #     # if event.buttons() & QtCore.Qt.RightButton:
-    #     #     delta = event.pos() - self.press
-    #     #     if delta.y() > -10:
-    #     #         scale = 1.01**delta.y()
-    #     #         print("delta", delta.y(), "scale", scale)
-    #     #         self.scale(scale, scale)
-    #     if event.buttons() & QtCore.Qt.LeftButton:
-    #         print("mouse moved while left button pressed")
-    #         delta = event.pos() - self.press
-    #         print("delta", delta)
-    #         self.translate(delta.x(), delta.y())
-    #         #self.translate(1, 1)
-    #         #event.accept()
-
-
+    def mouseMoveEvent(self, event):
+        if event.buttons() & QtCore.Qt.RightButton:
+            delta = event.pos() - self.press
+            if delta.y() > -10:
+                scale = 1.01**delta.y()
+                print("delta", delta.y(), "scale", scale)
+                #self.scale(scale, scale)
+       
     def resizeEvent(self, event):
-        print("tile window resizeEvent")
-        self.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+    #     pass
+        #print("tile window resizeEvent")
+        self.fitInView(self.scene().sceneRect(), QtCore.Qt.KeepAspectRatio)
         #self.scale(2,2)
         #event.accept()
+        return super().resizeEvent(event)
 
+class TabLoader(QtCore.QThread):
+    messageSignal = QtCore.pyqtSignal(list, str)
+
+    def __init__(self, prefix, r, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prefix = prefix
+        self.r = r
+
+
+
+    def run(self):
+        for t in self.r.counter.unique()[:-1]:
+            # print(t)
+            # Get all items in time t
+            d = self.r[self.r.counter == t]
+            result = []
+            for row in d.itertuples():
+                fname = os.path.join(self.prefix, row.fname)
+                print(row)
+                if row.k == 0:
+                    continue
+                x0 = row.k * FOV_X_PIXELS
+                y0 = row.j * FOV_Y_PIXELS
+                image = QtGui.QImage(fname)
+                result.append([image, x0, y0])
+            self.messageSignal.emit(result, str(t))
+                
+            
 
 class TabWidget(QtWidgets.QTabWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         g = glob.glob("movie/*")
         g.sort()
         prefix = g[-1]
@@ -119,61 +99,36 @@ class TabWidget(QtWidgets.QTabWidget):
         r=pd.read_json(f"{prefix}/tile_config.json", lines=True)
         r.set_index(['counter', 'i', 'j', 'k'])
 
-        for t in r.counter.unique()[:-1]:
-            # print(t)
-            # Get all items in time t
-            d = r[r.counter == t]
 
+        self.tab_loader = TabLoader(prefix, r, self)
+        self.tab_loader.messageSignal.connect(self.onTabLoaded)
+        self.tab_loader.start()
 
-            w = QtWidgets.QWidget()
-            layout = QtWidgets.QVBoxLayout()
-            w.setLayout(layout)
-            tw = TileWindow(prefix, d)
-            layout.addWidget(tw)
-            self.addTab(w, str(t))
-
-
-        self.dumpObjectTree()
-
-
-    def resizeEvent(self, event):
-        print("tab widget resizeEvent")
-        return super().resizeEvent(event)
-
-
-class CentralWidget(QtWidgets.QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        print("central widget")
-        
-    def resizeEvent(self, event):
-        print("central widget resizeEvent")
+    def onTabLoaded(self, images, label):
+        scene = QtWidgets.QGraphicsScene()
+        for image, x0, y0 in images:
+            pixmap = QtGui.QPixmap(image)
+            item = scene.addPixmap(pixmap)
+            item.setPos(x0, y0)
+            
+        tw = TileWindow()
+        tw.setScene(scene)
+        tw.setSceneRect(scene.sceneRect())
+        self.addTab(tw, label)
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         loadUi("stitcher/stitcher.ui", self)
-        self.setCentralWidget(self.centralwidget)
                 
-        
-    def resizeEvent(self, event):
-        print("main window  resize event")
-        self.centralWidget().resizeEvent(event)
-
 class QApplication(QtWidgets.QApplication):
     def __init__(self, *argv):
         super().__init__(*argv)
 
         self.main_window = MainWindow()
-        self.main_window.showMaximized()
-        self.installEventFilter(self)
-
-    def eventFilter(self, widget, event):
-        if isinstance(event, QtGui.QKeyEvent):
-            self.main_window.tabWidget.currentWidget().keyPressEvent(event)
-        return False
-
+        self.main_window.show()
+   
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
