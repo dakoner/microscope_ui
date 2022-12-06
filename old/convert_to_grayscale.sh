8a4472ece6ebc0b94f34328b6323a05051cda2fc
dek <dakoner@gmail.com>
2022-12-06 09:29:31 -0800

    Clean up.

--- old/convert_to_grayscale.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-convert -colorspace Gray $1 movie_grayscale/$(basename $1)
--- old/start_serial.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-source /home/dek/miniconda3/etc/profile.d/conda.sh
-conda activate microscope
-cd /home/dek/src/microscope_ui
-python serial_interface.py  /dev/ttyUSB0 dekscope
