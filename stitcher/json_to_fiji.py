01a94f254aace9484d7a386a4a1faafe15206a0a
David Konerding <dakoner@gmail.comwq>
2024-03-18 15:26:36 -0700

    updates

--- stitcher/json_to_fiji.py
+++ stitcher/json_to_fiji.py
@@ -10,20 +10,26 @@ def main():
     #g.sort()
     #prefix = g[-1]
     #d=json.load(open(f"{prefix}/scan_config.json"))
-    prefix = "photo/1709266494.86227"
+    import glob
+    g = glob.glob("photo/*")
+    prefix = sorted(g, key=lambda x: float(x.split("/")[1]))[-1]
     r=pd.read_json(f"{prefix}/tile_config.json", lines=True)
     r.set_index(['i', 'j', 'k'])
 
     f = open(f"{prefix}/TileConfiguration.txt", "w")
     f.write("dim=2\n")
 
+    first = True
     for row in r.itertuples():
         fname = row.fname
-        x0 = row.x/PIXEL_SCALE*0.9
-        y0 = row.y/PIXEL_SCALE*0.9
-        #x0 = row.k * FOV_X_PIXELS
-        #y0 = row.j * FOV_Y_PIXELS
-        f.write(f"{fname}; ; ({x0}, {y0})\n")
+        if first:
+            x_0 =  row.x/PIXEL_SCALE
+            y_0 = row.y/PIXEL_SCALE
+        x = row.x/PIXEL_SCALE - x_0
+        y = row.y/PIXEL_SCALE- y_0
+      
+        first = False
+        f.write(f"{fname}; ; ({x}, {y})\n")
 
 
 if __name__ == '__main__':
