b86e8fea737344cc1570d9e0763b959e9edd7fcf
David Konerding <dakoner@gmail.com>
2024-11-22 13:14:26 -0800

    Formatting

--- stitcher/stitch_test.py
+++ stitcher/stitch_test.py
@@ -6,19 +6,23 @@ from PIL import Image
 import sys
 import concurrent.futures
 
+
 def load_image(prefix, image):
     filename = f"{prefix}/{image.filename}"
     i = Image.open(filename)
     i = i.convert("L")
     return filename, image, i
 
+
 def main():
     prefix = sys.argv[1]
     tc = TileConfiguration()
     tc.load(f"{prefix}/TileConfiguration.txt")
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        future_to_image = {executor.submit(load_image, prefix, image): image for image in tc.images}
-        
+        future_to_image = {
+            executor.submit(load_image, prefix, image): image for image in tc.images
+        }
+
     polys = []
     for future in concurrent.futures.as_completed(future_to_image):
         fname = future_to_image[future]
@@ -28,12 +32,9 @@ def main():
             image.x, image.y, image.x + i.width, image.y + i.height
         )
         polys.append(b)
-        
-        
 
     c = shapely.geometry.MultiPolygon(polys)
     print("Full size", c.bounds)
 
 
 main()
-
