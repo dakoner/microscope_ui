242b2dd97579ae9eb88b43a8e67e201e1c18e38c
David Konerding <dakoner@gmail.com>
2024-11-23 22:11:42 -0800

    use tiffile, write scale metadata, round up image size to nearest CHUNK_SIZE multiple

--- stitcher/stitch_test.py
+++ stitcher/stitch_test.py
@@ -1,6 +1,6 @@
 from lxml import etree as ET
 import sys
-
+import tifffile
 sys.path.insert(0, "controller")
 import numpy as np
 import math
@@ -12,6 +12,7 @@ import sys
 import concurrent.futures
 import dask.array as da
 
+CHUNK_SIZE = 8192
 
 def load_image(prefix, image):
     filename = f"{prefix}/{image.filename}"
@@ -58,17 +59,58 @@ def render_intersection():
         w.write(ET.tostring(t).decode("utf-8"))
         w.write("</svg>")
 
+def do(polys, x, y, box_to_image):
+    b = shapely.geometry.box(x, y, x + CHUNK_SIZE, y + CHUNK_SIZE)
+    results = np.zeros((CHUNK_SIZE, CHUNK_SIZE), dtype=np.uint64)
+    counter = np.zeros((CHUNK_SIZE, CHUNK_SIZE), dtype=np.uint8)
+    intersecting = []
+    for p in polys:
+        if b.intersects(p):
+            intersecting.append(p)
+    for p in intersecting:
+        inter = b.intersection(p, grid_size=1)
+        box_origin = sorted(p.exterior.coords)[0]
+        inter_in_box_coords = shapely.transform(
+            inter, lambda x: x - box_origin
+        ).bounds
+        chunk_origin = x, y
+        inter_in_chunk_coords = shapely.transform(
+            inter, lambda x: x - chunk_origin
+        ).bounds
 
+        im = box_to_image[p]
+        data = im[
+            int(inter_in_box_coords[0]) : int(inter_in_box_coords[2]),
+            int(inter_in_box_coords[1]) : int(inter_in_box_coords[3]),
+        ]
+        results[
+            int(inter_in_chunk_coords[0]) : int(inter_in_chunk_coords[2]),
+            int(inter_in_chunk_coords[1]) : int(inter_in_chunk_coords[3]),
+        ] += data
+        counter[int(inter_in_chunk_coords[0]) : int(inter_in_chunk_coords[2]),
+            int(inter_in_chunk_coords[1]) : int(inter_in_chunk_coords[3])] += 1
+    
+    results = results / counter
+    results[np.isnan(results)] = 0
+    return results
+    # im = Image.fromarray(np.uint8(counter))
+    # im.save(f"out\\counter.{i},{j}.png")
+    # im = Image.fromarray(np.uint8(results))
+    # im.save(f"out\\chunk.{i},{j}.png")
+
+def round_up(val):
+    x = int(val / CHUNK_SIZE)
+    return CHUNK_SIZE * (x+1)
+    
 def main(prefix):
     tc = TileConfiguration()
-    tc.load(f"{prefix}/TileConfiguration.registered.registered.txt")
+    tc.load(f"{prefix}/TileConfiguration.registered.txt")
     tc.move_to_origin()
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         future_to_image = {
             executor.submit(load_image, prefix, image): image for image in tc.images
         }
 
-    box_to_fname = {}
     box_to_image = {}
 
     polys = []
@@ -78,83 +120,30 @@ def main(prefix):
         b = shapely.geometry.box(
             int(image.x), int(image.y), int(image.x) + i.width, int(image.y) + i.height
         )
-        box_to_fname[b] = filename
         box_to_image[b] = np.asarray(i).T
         polys.append(b)
 
     c = shapely.geometry.GeometryCollection(polys)
-    # with open("test.svg", "w") as w:
-    #     w.write('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">')
-    #     w.write(c.svg())
-    #     w.write('</svg>')
-
-    print("Full size", c.bounds)
 
     # iterate over chunks from the full size image
     # find all the intersecting polys
     # composite the chunk from the intersecting polys
-    CHUNK_SIZE = 2000
-    d = da.zeros((c.bounds[2], c.bounds[3]), dtype=np.uint8)
-    for i, x in enumerate(range(0, int(c.bounds[2]), CHUNK_SIZE)):
-        for j, y in enumerate(range(0, int(c.bounds[3]), CHUNK_SIZE)):
-            b = shapely.geometry.box(x, y, x + CHUNK_SIZE, y + CHUNK_SIZE)
-            print("i,j", i, j, x, y)
-            print("Chunk", b.bounds)  # chunk in pixel coordinates
-            results = np.zeros((CHUNK_SIZE, CHUNK_SIZE), dtype=np.uint64)
-            counter = np.zeros((CHUNK_SIZE, CHUNK_SIZE), dtype=np.uint8)
-            intersecting = []
-            for p in polys:
-                if b.intersects(p):
-                    intersecting.append(p)
-            for p in intersecting:
-                inter = b.intersection(p, grid_size=1)
-                box_origin = sorted(p.exterior.coords)[0]
-                inter_in_box_coords = shapely.transform(
-                    inter, lambda x: x - box_origin
-                ).bounds
-                chunk_origin = x, y
-                inter_in_chunk_coords = shapely.transform(
-                    inter, lambda x: x - chunk_origin
-                ).bounds
-                # print("\tinter", inter)
-                # print("\tPolygon", p)
-                # print("\tbox_origin", box_origin)
-                # print("\tinter in box coords", inter_in_box_coords)
-                # print("\tchunk_origin", chunk_origin)
-                # print("\tinter in chunk coords", inter_in_box_coords)
-
-                im = box_to_image[p]
-                data = im[
-                    int(inter_in_box_coords[0]) : int(inter_in_box_coords[2]),
-                    int(inter_in_box_coords[1]) : int(inter_in_box_coords[3]),
-                ]
-                results[
-                    int(inter_in_chunk_coords[0]) : int(inter_in_chunk_coords[2]),
-                    int(inter_in_chunk_coords[1]) : int(inter_in_chunk_coords[3]),
-                ] += data
-                counter[int(inter_in_chunk_coords[0]) : int(inter_in_chunk_coords[2]),
-                    int(inter_in_chunk_coords[1]) : int(inter_in_chunk_coords[3])] += 1
-            im = Image.fromarray(np.uint8(counter))
-            im.save(f"out\\counter.{i},{j}.png")
-            results = results / counter
-            results[np.isnan(results)] = 0
-
-            im = Image.fromarray(np.uint8(results))
-            im.save(f"out\\chunk.{i},{j}.png")
-
-            if x + CHUNK_SIZE > d.shape[0]:
-                max_x = d.shape[0]
-            else:
-                max_x = x + CHUNK_SIZE
-            if y + CHUNK_SIZE > d.shape[1]:
-                max_y = d.shape[1]
-            else:
-                max_y = y + CHUNK_SIZE
-
-            d[x:max_x, y:max_y] = results[: max_x - x, : max_y - y]
-    im = Image.fromarray(d.compute())
-    im.save("final.png")
+    bounds = round_up(c.bounds[2]), round_up(c.bounds[3])
+    d = da.zeros(bounds, dtype=np.uint8)
+    
+    for x in range(0, int(c.bounds[2]), CHUNK_SIZE):
+        for y in range(0, int(c.bounds[3]), CHUNK_SIZE):
+            results = do(polys, x, y, box_to_image)
 
 
-# main(sys.argv[1])
-main("controller\\photo\\1732319758.459453")
+            d[x:x+CHUNK_SIZE, y:y+CHUNK_SIZE] = results
+            del results
+    print("Write final image")
+    tifffile.imwrite('temp.ome.tif', d, imagej=True, resolution=(832, 832), metadata={'unit': 'mm', 'axes': 'YX'})
+
+
+
+#main(sys.argv[1])
+main("controller\\photo\\1732256851.6429064")
+#main("controller\\photo\\1732319758.459453")
+
