5d93b38989744b27f8923f9d58e8482515065a25
David Konerding <dakoner@gmail.com>
2024-12-02 17:57:03 -0800

    remove obsolete scripts

--- stitcher/to_zarr.py
+++
@@ -1,7 +0,0 @@
-import dask.array as da
-import numpy as np
-from numcodecs import Blosc
-import tifffile
-fname = "test.tif"
-d = tifffile.imread(fname)
-da.array(np.moveaxis(d, 2, 0)).to_zarr("temp.zarr", compressor=Blosc(cname='zstd', clevel=3))
