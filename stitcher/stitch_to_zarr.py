4a21494e56ec24e9dbffa89852d8cc116e75e17a
David Konerding <dakoner@gmail.com>
2024-12-07 14:40:11 -0800

    Use safe but tested update-in-the-main-thread

--- stitcher/stitch_to_zarr.py
+++ stitcher/stitch_to_zarr.py
@@ -15,62 +15,101 @@ import numpy as np
 import math
 import time
 import shapely, shapely.geometry, shapely.strtree
-from tile_configuration import TileConfiguration, tile_config_to_tileconfiguration
+from tile_configuration import (
+    TileConfiguration,
+    tile_config_to_tileconfiguration,
+    tile_config_to_shapely,
+)
 from PIL import Image
 import sys
 import concurrent.futures
 from util import get_image_data, get_image_dimensions, CHUNK_SIZE
-import threading
 
-# l = threading.Lock()
 
-# def round_up(val):
-#     x = int(val / CHUNK_SIZE)
-#     return CHUNK_SIZE * (x + 1)
+def fn(prefix, image):
+    try:
+        filename = pathlib.Path(prefix) / image.filename
+        x = int(image.x)
+        y = int(image.y)
+        img = get_image_data(filename).swapaxes(0,1)
 
+        width = img.shape[0]
+        height = img.shape[1]
+    except Exception as exc:
+        print('%r generated an exception: %s' % (image, exc))
+        return None
+    return filename, img, x, y, width, height
 
 def main(prefix):
     tc = TileConfiguration()
-    tc.load(f"{prefix}/TileConfiguration.registered.registered.txt")
+    fname = prefix / "TileConfiguration.txt"
+    if not fname.exists():
+        tile_config_to_tileconfiguration(prefix)
+    tc.load(fname)
     tc.move_to_origin()
-
-
+    c = tile_config_to_shapely(prefix, tc)
+    print(c.bounds)
     z = zarr.open(
         "test.zarr",
         mode="w",
-        shape=(3, c.bounds[2], c.bounds[3]),
-        chunks=(3, CHUNK_SIZE, CHUNK_SIZE),
+        shape=(c.bounds[2], c.bounds[3], 3),
+        chunks=(CHUNK_SIZE, CHUNK_SIZE, 3),
         dtype=np.uint16,
+        synchronizer=zarr.ThreadSynchronizer()
     )
     counter = zarr.open(
         "counter.zarr",
         mode="w",
-        shape=(3, int(c.bounds[2]), int(c.bounds[3])),
-        chunks=(3, CHUNK_SIZE, CHUNK_SIZE),
+        shape=(c.bounds[2], c.bounds[3], 3),
+        chunks=(CHUNK_SIZE, CHUNK_SIZE, 3),
         dtype=np.uint8,
+    
+        synchronizer=zarr.ThreadSynchronizer()
     )
 
-    for image in tc.images:#tqdm.tqdm(tc.images):
-        filename = pathlib.Path(prefix) / image.filename
-        img = get_image_data(filename)
-        x = int(image.x)
-        y = int(image.y)
-        width = img.shape[1]
-        height = img.shape[0]
-        z[:, x : x + width, y : y + height] += np.moveaxis(img.swapaxes(0,1), 2, 0)
-        counter[: , x : x + width, y : y + height] += 1
-
-    z = dask.array.from_array(z, chunks=(3, CHUNK_SIZE, CHUNK_SIZE))
-    counter = dask.array.from_array(counter, chunks=(3, CHUNK_SIZE, CHUNK_SIZE))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=128) as executor:
+        futures = []
+        for image in tc.images:
+            futures.append(executor.submit(fn, prefix, image))
+        print("Submitted all futures")
+        l = len(futures)
+        t = tqdm.tqdm(total=l)
+        for i, future in enumerate(concurrent.futures.as_completed(futures)):
+            #print("Completed", i, "of", l)
+            
+            r = future.result()
+            if r:
+                filename, img, x, y, width, height = r 
+            
+                z[x : x + width, y : y + height, : ] += img
+                counter[x : x + width, y : y + height, :] += 1    
+         
+            t.update(1)
+            
+    # for image in tc.images:
+    #     print(image)
+    #     filename, img, x, y, width, height = fn(prefix, image)
+    #     print(filename, x, y, width, height)
+    #     z[x : x + width, y : y + height] += img
+    #     counter[x : x + width, y : y + height] += 1
+    #     #r = future.result()
+    # #         futures.append(executor.submit(fn, prefix, image, z, counter))
+    #     #t.close()
+    z = dask.array.from_array(z, chunks=(CHUNK_SIZE, CHUNK_SIZE, 3))
+    counter = dask.array.from_array(counter, chunks=(CHUNK_SIZE, CHUNK_SIZE, 3))
     z = z // counter
+
+
+
     z = z.astype(dask.array.uint8)
-    dask.array.to_zarr(z, "test-2.zarr")
+    print(z)
+    dask.array.to_zarr(dask.array.moveaxis(z, 2, 0), "test-2.zarr")
 
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    prefix = sys.argv[1]
-    #tile_config_to_tileconfiguration(prefix)
+    prefix = pathlib.Path(sys.argv[1])
+    # tile_config_to_tileconfiguration(prefix)
     # prefix = r"C:\Users\davidek\microscope_ui\controller\photo\1733112316.740324"
     main(prefix)
     # main("C:\\Users\\davidek\\Desktop\\1732488657.752864")
